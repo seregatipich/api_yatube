5c0972ac65f48a829b85262463e51208a01aa3c8
Sergei Poluektov <poluektovsergei345@gmail.com>
2022-11-12 15:47:48 +0100

    fixed some issues

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,26 +7,6 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def perform_create(self, serializer):
-        if self.request.user and self.request.auth:
-            serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super().perform_update(serializer)
-        return Response(status=status.HTTP_200_OK)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        super().perform_destroy(instance)
-
-
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
@@ -40,7 +20,8 @@ class PostViewSet(viewsets.ModelViewSet):
         permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        if self.request.user and self.request.auth:
+            serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
